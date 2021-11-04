915d8785a44d9e71635e032c0702a9e5026ea1c7
KevinJeanMarie <kjeanmarie01@gmail.com>
2021-11-04 20:35:20 +0100

    modif

--- session1/variables-1/main.js
+++ session1/variables-1/main.js
@@ -2,12 +2,16 @@ console.log('Bienvenue dans votre premier exercice sur JavaScript !');
 
 // TODO: ÉTAPE 1
 // Afficher dans la console le mois actuel
+var month = "novembre";
+console.log(month)
 // en assignant la bonne valeur à la variable `month` ci-dessous.
 // Libre à vous de choisir le format : soit le numéro, soit le nom du mois.
 
-var month;
+
 
 // TODO: ÉTAPE 2
+month = "decembre"
+console.log(month)
 // Afficher dans la console le mois prochain
 // en réassignant la variable `month` ci-dessus,
 // donc sans supprimer le code de l'étape 1 !
